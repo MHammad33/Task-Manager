60a4789755c323dcbb41a641e01dc647ac9914d7
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-07-11 11:27:16 +0500

    Initial Backend Project Setup

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
-console.log("Task Manager");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+})
+
+// app.get("/api/v1/tasks")         -- get all the tasks
+// app.post("/api/v1/tasks")        -- create a new task
+// app.get("/api/v1/tasks/:id")     -- get a single task
+// app.patch("/api/v1/tasks/:id")   -- update a task
+// app.delete("/api/v1/tasks/:id")  -- delete a task
+
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
