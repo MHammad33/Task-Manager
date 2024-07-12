8db44ed81a2287d1c305a6218519d69ff78f8b12
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-07-12 10:11:56 +0500

    Create Task and add to the database

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -1,7 +1,12 @@
 const Task = require("../models/Task.model");
 
 const getAllTasks = (req, res) => { res.send("Tasks") }
-const createTask = (req, res) => { res.json(req.body) }
+
+const createTask = async (req, res) => {
+  const task = await Task.create(req.body);
+  res.status(201).json({ task });
+}
+
 const getTask = (req, res) => { res.json({ id: req.params.id }) }
 const updateTask = (req, res) => { res.json({ id: req.params.id }) }
 const deleteTask = (req, res) => { res.json({ id: req.params.id }) }
