b26f0ceb90d61a351aafc6bebbf40cdffc8d9392
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-07-12 10:19:42 +0500

    Add try/catch to handle errors gracefully

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -3,8 +3,12 @@ const Task = require("../models/Task.model");
 const getAllTasks = (req, res) => { res.send("Tasks") }
 
 const createTask = async (req, res) => {
-  const task = await Task.create(req.body);
-  res.status(201).json({ task });
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
 }
 
 const getTask = (req, res) => { res.json({ id: req.params.id }) }
