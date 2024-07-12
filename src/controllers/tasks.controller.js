612d8fba3f5e5791da8aa502d12f0bdb15bb2519
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-07-12 10:00:15 +0500

    Task Model

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -1,3 +1,5 @@
+const Task = require("../models/Task.model");
+
 const getAllTasks = (req, res) => { res.send("Tasks") }
 const createTask = (req, res) => { res.json(req.body) }
 const getTask = (req, res) => { res.json({ id: req.params.id }) }
