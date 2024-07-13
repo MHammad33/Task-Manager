fe24d51ebe069aa834575091c821466c44e19f1f
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-07-13 10:41:28 +0500

    Update Task with req.body

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -30,6 +30,12 @@ const createTask = async (req, res) => {
   }
 }
 
+/**
+ * 
+ * @param {*} req 
+ * @param {*} res 
+ * @returns Task by id
+ */
 const getTask = async (req, res) => {
   try {
 
@@ -48,7 +54,34 @@ const getTask = async (req, res) => {
 }
 
 
-const updateTask = (req, res) => { res.json({ id: req.params.id }) }
+/**
+ * 
+ * @param {*} req 
+ * @param {*} res 
+ * @returns Updated task
+ */
+const updateTask = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const task = await Task.findByIdAndUpdate(id, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    // Check if task exists
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${id}` });
+    }
+
+    res.status(200).json({ task });
+
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
+
+}
+
 const deleteTask = (req, res) => { res.json({ id: req.params.id }) }
 
 module.exports = {
